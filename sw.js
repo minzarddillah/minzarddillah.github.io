8c3da392e636a94549e5f1e941a64371ddcb5235
Minzard <39266818+minzarddillah@users.noreply.github.com>
2023-07-18 02:15:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -95,29 +95,6 @@ self.addEventListener(`message`, event => {
   if (api) MessageAPI[api](event, event.data)
 })
 
-self.addEventListener('install', function(e) {
-
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function (cacheName) {
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
-          })
-          .map(function (cacheName) {
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
-})
-
 function handleAPIRequest({ event }) {
   const { pathname } = new URL(event.request.url)
 
